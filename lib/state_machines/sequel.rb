1ac69776166ec8526c5c2641be129f6ef73341c5
Michael Granger <ged@FaerieMUD.org>
2016-03-10 11:15:29 -0800

    Initial commit.

---
+++ lib/state_machines/sequel.rb
@@ -0,0 +1,15 @@
+# -*- ruby -*-
+#encoding: utf-8
+
+# Toplevel namespace
+module Statemachines::Sequel
+
+	# Package version
+	VERSION = '0.0.1'
+
+	# Version control revision
+	REVISION = %q$Revision: ed192967e2af $
+
+
+end # module Statemachines::Sequel
+
---
+++ spec/state_machines/sequel_spec.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env rspec -cfd
+#encoding: utf-8
+
+require_relative '../spec_helper'
+
+require 'rspec'
+require 'state_machines/sequel'
+
+describe Statemachines::Sequel do
+
+	
+
+end
+
